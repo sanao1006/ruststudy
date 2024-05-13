8636e76ad562dca0746ddef6a96377eb60cb3bc3
sanao1006 <naosak1006@gmail.com>
2024-05-13 22:30:58 +0900

    10.6

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 mod cahp8;
 
-const DIGEST_SIZE: usize = 3;
-const ZERO: Option<u8> = Some(42);
-
-fn compute_digest(text: &str) -> [u8; DIGEST_SIZE] {
-    let mut digest = [ZERO.unwrap_or(0); DIGEST_SIZE];
-    for (idx, &b) in text.as_bytes().iter().enumerate() {
-        digest[idx % DIGEST_SIZE] = digest[idx % DIGEST_SIZE].wrapping_add(b)
-    }
-    digest
+enum CarryableConcreteItem {
+    Left,
+    Right
 }
 
+type Item = CarryableConcreteItem;
+
+use std::cell::RefCell;
+use std::sync::{Arc, RwLock};
+
+type PlayerInventory = RwLock<Vec<Arc<RefCell<Item>>>>;
+
 fn main() {
-    let digest = compute_digest("Hello");
-    println!("digest: {digest:?}");
+
 }
