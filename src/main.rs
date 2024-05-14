c998ba3281200a49e1783fd7626a0eea6dc50e92
sanao1006 <naosak1006@gmail.com>
2024-05-14 22:28:34 +0900

    12.3

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use crate::chap10::{car_arrived, car_door_closed, car_door_opened, car_floor_but
 mod cahp8;
 mod chap10;
 
-struct Foo {
-    x: (u32, u32),
-    y:u32,
+enum Result {
+    Ok(i32),
+    Err(String),
+}
+
+fn devide_in_two(n: i32) -> Result {
+    if n % 2 == 0 {
+        Result::Ok(n / 2)
+    } else {
+        Result::Err(format!("cannot divide {n} into two equal parts"))
+    }
 }
 
 fn main() {
-    let foo = Foo { x: (2, 2), y: 2};
-    match foo {
-        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
-        Foo {y: 2, x: i} => println!("y = 2, x = {i:?}"),
-        Foo { y, .. } => println!("y = {y}, other fields were ignored")
+    let n = 100;
+    match devide_in_two(n) {
+        Result::Ok(half) => println!("{n} divided in two is {half}"),
+        Result::Err(msg) => println!("sorry, an error happened: {msg}"),        
     }
 }
