13d64c23b54ea05e67ada9ffe823ae8b0878f4db
sanao1006 <naosak1006@gmail.com>
2024-05-14 22:11:53 +0900

    12.1

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,14 @@ use crate::chap10::{car_arrived, car_door_closed, car_door_opened, car_floor_but
 mod cahp8;
 mod chap10;
 
+
 fn main() {
-    println!(
-        "A ground floor passenger has pressed the up button: {:?}",
-        lobby_call_button_pressed(0, Direction::Up)
-    );
-    println!("The car has arrived on the ground floor: {:?}", car_arrived(0));
-    println!("The car door opened: {:?}", car_door_opened());
-    println!(
-        "A passenger has pressed the 3rd floor button: {:?}",
-        car_floor_button_pressed(3)
-    );
-    println!("The car door closed: {:?}", car_door_closed());
-    println!("The car has arrived on the 3rd floor: {:?}", car_arrived(3));
+    let input = 'x';
+    match input {
+        'q'  => print!("Quitting"),
+        'a' | 's' | 'w' | 'd' => println!("Moving around"),
+        '0'..='9' => println!("Number Input"),
+        key if key.is_lowercase() => println!("Lowercase: {key}").to_owned(),
+        _ => println!("Something else"),
+    }
 }
