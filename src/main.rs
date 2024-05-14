10e8d4ebb55494be2bf3e804823de8c4156409a5
sanao1006 <naosak1006@gmail.com>
2024-05-14 22:32:04 +0900

    12.4

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
+use std::time::Duration;
+
 use crate::chap10::{car_arrived, car_door_closed, car_door_opened, car_floor_button_pressed, lobby_call_button_pressed, Direction};
 
 mod cahp8;
 mod chap10;
 
-enum Result {
-    Ok(i32),
-    Err(String),
-}
-
-fn devide_in_two(n: i32) -> Result {
-    if n % 2 == 0 {
-        Result::Ok(n / 2)
-    } else {
-        Result::Err(format!("cannot divide {n} into two equal parts"))
+fn sleep_for(secs: f32) {
+    if let Ok(dur) = Duration::try_from_secs_f32(secs)  {
+        std::thread::sleep(dur);
+        println!("slept for {:?}", dur)
+        
     }
 }
 
 fn main() {
-    let n = 100;
-    match devide_in_two(n) {
-        Result::Ok(half) => println!("{n} divided in two is {half}"),
-        Result::Err(msg) => println!("sorry, an error happened: {msg}"),        
-    }
+    sleep_for(-10.0);
+    sleep_for(0.8)
 }
