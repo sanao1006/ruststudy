1a9d6f0bf90bf790ffcdd52c61730d1dec047447
sanao1006 <naosak1006@gmail.com>
2024-05-14 22:21:57 +0900

    12.2

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use crate::chap10::{car_arrived, car_door_closed, car_door_opened, car_floor_but
 mod cahp8;
 mod chap10;
 
+struct Foo {
+    x: (u32, u32),
+    y:u32,
+}
 
 fn main() {
-    let input = 'x';
-    match input {
-        'q'  => print!("Quitting"),
-        'a' | 's' | 'w' | 'd' => println!("Moving around"),
-        '0'..='9' => println!("Number Input"),
-        key if key.is_lowercase() => println!("Lowercase: {key}").to_owned(),
-        _ => println!("Something else"),
+    let foo = Foo { x: (2, 2), y: 2};
+    match foo {
+        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
+        Foo {y: 2, x: i} => println!("y = 2, x = {i:?}"),
+        Foo { y, .. } => println!("y = {y}, other fields were ignored")
     }
 }
