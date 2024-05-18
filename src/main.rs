cd718bfcc98f366ee315c4c109728fbc03dc90b4
sanao1006 <naosak1006@gmail.com>
2024-05-18 22:15:56 +0900

    13.1

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,41 @@ mod cahp8;
 mod chap10;
 mod chap12;
 
-fn sleep_for(secs: f32) {
-    if let Ok(dur) = Duration::try_from_secs_f32(secs)  {
-        std::thread::sleep(dur);
-        println!("slept for {:?}", dur)
-        
+
+#[derive(Debug)]
+struct Race {
+    name: String,
+    laps: Vec<i32>,
+}
+
+impl Race {
+    fn new(name: &str) -> Self {
+        Self { name: String::from(name), laps: Vec::new() }
+    }
+
+    fn add_lap(&mut self, lap: i32) {
+        self.laps.push(lap)
+    }
+
+    fn print_laps(&self) {
+        println!("Recorded {} laps for {}:", self.laps.len(), self.name);
+        for (idx, lap) in self.laps.iter().enumerate() {
+            println!("Lap {idx}: {lap} sec");
+        }
+    }
+
+    fn finish(self) {
+        let total: i32 = self.laps.iter().sum();
+        println!("Race {} is finished, total lap time: {}", self.name, total);
     }
 }
 
 fn main() {
-    sleep_for(-10.0);
-    sleep_for(0.8)
+    let mut race = Race::new("Monaco Grand Prix");
+    race.add_lap(32);
+    race.add_lap(68);
+    race.print_laps();
+    race.add_lap(71);
+    race.print_laps();
+    race.finish();
 }
