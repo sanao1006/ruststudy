3482bfabfaa33043e6ba51974ba337a8262f04ba
sanao1006 <naosak1006@gmail.com>
2024-05-18 22:28:59 +0900

    13.2.1

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,26 @@ mod chap10;
 mod chap12;
 
 
-#[derive(Debug)]
-struct Race {
-    name: String,
-    laps: Vec<i32>,
+trait Pet {
+    fn talk(&self) -> String;
+
+    fn greet(&self) {
+        println!("Oh you're a cutie! What's your name? {}", self.talk());
+    }
 }
 
-impl Race {
-    fn new(name: &str) -> Self {
-        Self { name: String::from(name), laps: Vec::new() }
-    }
+struct Dog {
+    name: String,
+    age: i8,
+}
 
-    fn add_lap(&mut self, lap: i32) {
-        self.laps.push(lap)
-    }
-
-    fn print_laps(&self) {
-        println!("Recorded {} laps for {}:", self.laps.len(), self.name);
-        for (idx, lap) in self.laps.iter().enumerate() {
-            println!("Lap {idx}: {lap} sec");
-        }
-    }
-
-    fn finish(self) {
-        let total: i32 = self.laps.iter().sum();
-        println!("Race {} is finished, total lap time: {}", self.name, total);
+impl Pet for Dog {
+    fn talk(&self) -> String {
+        format!("Woof, my name is {}!", self.name)
     }
 }
 
 fn main() {
-    let mut race = Race::new("Monaco Grand Prix");
-    race.add_lap(32);
-    race.add_lap(68);
-    race.print_laps();
-    race.add_lap(71);
-    race.print_laps();
-    race.finish();
+    let dog = Dog { name: String::from("Fido"), age: 5 };
+    dog.greet();
 }
